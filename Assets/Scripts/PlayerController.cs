673039250485ed2df4fb195172d1a52df05cea49
Faris Ahmed T. Alotaibi <faa339@nyu.edu>
2023-02-17 15:54:08 -0500

    change: PlayerController (hello)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("Beginning!");
+        print("Hello!");
     }
 
     // Update is called once per frame
