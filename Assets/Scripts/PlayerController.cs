1651618ef235052b6160de390f3160088602f5e3
Faris Ahmed T. Alotaibi <faa339@nyu.edu>
2023-02-17 15:56:25 -0500

    Revert "change: PlayerController (print change)"
    This reverts commit 128460d7fd5ed740d3185aef7920c66934e59a5e.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("Hi!");
+        print("Beginning!");
     }
 
     // Update is called once per frame
